9924427f3ea4b875dc4827d910bbfc087667b3ff
elias <hliasvarelas3113@gmail.com>
2021-07-31 00:25:10 +0300

    some js

--- mywork/patientinfo-bootstrap.php
+++ mywork/patientinfo-bootstrap.php
@@ -123,8 +123,8 @@
                             <td><?php echo $row['Phonenum'] ; ?></td>
                             <td><?php echo $row['Email']; ?></td>
                             <td><?php echo "<a href='/previousvisit-bootstrap.php?id=".$row['Patient_id']."'>Previous Visits</a>"; ?></td>
-                            <td><?php echo "<a href='/Multiple_Sclerosis_app.php?id=".$row['Patient_id']. "&?nm=". $row['Patient_name'] ."'>Add Follow up</a>"; ?></td> <!-- Passes the patients id in the form for minimazing user error -->
-                            <td><button id="removeuser"><?php echo "<a href='/removeuser.php?id=".$row['Patient_id']."'>Remove Patient</a>"; ?></button></td>  <!-- Removes only the patient with the particular id -->
+                            <td><?php echo "<a href='/Multiple_Sclerosis_app.php?id=".$row['Patient_id']. "&nm=". $row['Patient_name'] ."'>Add Follow up</a>"; ?></td> <!-- Passes the patients id in the form for minimazing user error -->
+                            <td><button id="removeuser" onclick="remove_user"><?php echo "<?id=".$row['Patient_id']."'>Remove Patient</a>"; ?></button></td>  <!-- Removes only the patient with the particular id -->
                           </tr>
                     <?php
                     }
